9256af84f7ee9f95486bd8e828d1c4a54250ee34
michele <mikechiave99@gmail.com>
2022-06-18 15:32:39 +0200

    Architecture2222

--- app/src/main/java/com/example/network4/network/dto/AirQualityProvider.kt
+++ app/src/main/java/com/example/network4/network/dto/AirQualityProvider.kt
@@ -3,21 +3,20 @@ package com.example.network4.network.dto
 
 import com.example.network4.HeaderInterceptor
 import com.example.network4.MyApplication
-
-import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class AirQualityProvider {
 
-    val okHttpClient={
+    val httpclient: OkHttpClient.Builder=OkHttpClient.Builder()
+    httpclient.addInterceptor(HeaderInterceptor()){
         OkHttpClient.Builder()
         .addInterceptor(MyApplication,HeaderInterceptor())
         .build()
     }
     val retrofit = Retrofit.Builder()
-        .client(okHttpClient)
+        .client(httpclient)
         .baseUrl("https://air-quality.p.rapidapi.com")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
--- app/src/main/java/com/example/network4/network/dto/LicenseDto.kt
+++ app/src/main/java/com/example/network4/network/dto/LicenseDto.kt
@@ -1,8 +1,8 @@
 package com.example.network4.network.dto
 
-data class LicenseDto {
-    val E_BASE_URL = String
-    val API_ID = String
-    val API_KEY = String
-    val RAPIDAPI_KEY = String
-}
+data class LicenseDto (
+    val E_BASE_URL: String,
+    val API_ID :String,
+    val API_KEY: String,
+    val RAPIDAPI_KEY :String
+)
