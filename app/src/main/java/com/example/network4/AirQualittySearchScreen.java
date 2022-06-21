a4e6d2a17506de696ce4e30ee70cb3d37852974c
michele <mikechiave99@gmail.com>
2022-06-21 10:40:23 +0200

    ViewMOdelFirstCommit

---
+++ app/src/main/java/com/example/network4/AirQualittySearchScreen.java
@@ -0,0 +1,4 @@
+package com.example.network4;
+
+public class AirQualittySearchScreen {
+}
--- app/src/main/java/com/example/network4/MainActivity.kt
+++ app/src/main/java/com/example/network4/MainActivity.kt
@@ -12,8 +12,9 @@ import com.example.network4.MyApplication
 import com.example.network4.R
 import com.example.network4.network.dto.RepoDto
 import kotlinx.coroutines.launch
+import com.example.network4.ActivityMainBinding
 
-class AirQualitySearchScreen:AppCompatActivity(){
+class MainActivity:AppCompatActivity(){
     val firsttext = findViewById(R.id.textview_first) as TextView
     val errortext=findViewById(R.id.textview_error) as TextView
     private lateinit var viewModel: MainActivityViewModel
@@ -29,22 +30,22 @@ class AirQualitySearchScreen:AppCompatActivity(){
 
 
         val wheatherObserver =Observer<RepoDto> { newWheater ->
-                    firsttext.text = newWheater.city_name + "\n\n" + newWheater.lon.toString() +
-                            "\n\n" + newWheater.timezone + "\n\n" + newWheater.lat.toString() + "\n\n" +
-                            newWheater.country_code + "\n\n" + newWheater.state_code + "\n\n" + newWheater.data[0].mold_level +
-                            "\n\n" + newWheater.data[0].aqi + "\n\n" + newWheater.data[0].pm10 + "\n\n" + newWheater.data[0].co +
-                            "\n\n" + newWheater.data[0].o3 + "\n\n" + newWheater.data[0].predominant_pollen_type +
-                            "\n\n" + newWheater.data[0].so2 + "\n\n" + newWheater.data[0].pollen_level_tree + "\n\n" +
-                            newWheater.data[0].pollen_level_weed + "\n\n" + newWheater.data[0].no2 +
-                            "\n\n" + newWheater.data[0].pm25 + "\n\n" + newWheater.data[0].pollen_level_grass
-                    errortext.text = "No Error"
-                }
+            firsttext.text = newWheater.city_name + "\n\n" + newWheater.lon.toString() +
+                    "\n\n" + newWheater.timezone + "\n\n" + newWheater.lat.toString() + "\n\n" +
+                    newWheater.country_code + "\n\n" + newWheater.state_code + "\n\n" + newWheater.data[0].mold_level +
+                    "\n\n" + newWheater.data[0].aqi + "\n\n" + newWheater.data[0].pm10 + "\n\n" + newWheater.data[0].co +
+                    "\n\n" + newWheater.data[0].o3 + "\n\n" + newWheater.data[0].predominant_pollen_type +
+                    "\n\n" + newWheater.data[0].so2 + "\n\n" + newWheater.data[0].pollen_level_tree + "\n\n" +
+                    newWheater.data[0].pollen_level_weed + "\n\n" + newWheater.data[0].no2 +
+                    "\n\n" + newWheater.data[0].pm25 + "\n\n" + newWheater.data[0].pollen_level_grass
+            errortext.text = "No Error"
+        }
 
         val errorObserver= Observer<Exception> { newError ->
-                errortext.text = newError.toString()
-                firsttext.text ="No Data"
-            }
+            errortext.text = newError.toString()
+            firsttext.text ="No Data"
+        }
         viewModel.listWeather.observe(this, wheatherObserver)
         viewModel.listError.observe(this, errorObserver)
 
-        }}
+    }}
